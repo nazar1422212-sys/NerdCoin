882e377ef9c9acc8f37bb48f0652f2bf013d7d15
nazar1422212-sys <nazar1422212@gmail.com>
2026-06-01 20:44:43 +0300

    Comment out seed nodes in chainparams.cpp
    Commented out seed nodes for clarity and future updates.

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -150,15 +150,16 @@ public:
         // possible options.
         // This is fine at runtime as we'll fall back to using them as an addrfetch if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
-        // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seed.bitcoin.sipa.be."); // Pieter Wuille, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("dnsseed.bluematt.me."); // Matt Corallo, only supports x9
-        vSeeds.emplace_back("seed.bitcoin.jonasschnelli.ch."); // Jonas Schnelli, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("seed.btc.petertodd.net."); // Peter Todd, only supports x1, x5, x9, and xd
-        vSeeds.emplace_back("seed.bitcoin.sprovoost.nl."); // Sjors Provoost
-        vSeeds.emplace_back("dnsseed.emzy.de."); // Stephan Oeste
-        vSeeds.emplace_back("seed.bitcoin.wiz.biz."); // Jason Maurice
-        vSeeds.emplace_back("seed.mainnet.achownodes.xyz."); // Ava Chow, only supports x1, x5, x9, x49, x809, x849, xd, x400, x404, x408, x448, xc08, xc48, x40c
+        // release ASAP to avoid it where possible.  
+// vSeeds.emplace_back("seed.bitcoin.sipa.be.");
+// vSeeds.emplace_back("dnsseed.bluematt.me.");
+// vSeeds.emplace_back("seed.bitcoin.jonasschnelli.ch.");
+// vSeeds.emplace_back("seed.btc.petertodd.net.");
+// vSeeds.emplace_back("seed.bitcoin.sprovoost.nl.");
+// vSeeds.emplace_back("dnsseed.emzy.de.");
+// vSeeds.emplace_back("seed.bitcoin.wiz.biz.");
+// vSeeds.emplace_back("seed.mainnet.achownodes.xyz.");
+
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
